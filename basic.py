d20e37e8647f917856f7945dd031f0720e3a2be4
Apurv Padir <apurvpadir01@gmail.com>
2026-08-18 01:10:50 +0530

    Python code file added

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
+# Writing a code in Python to find out the largest number among the given three numbers.
+# The numbers are given input by the user.
 a = int(input("Enter number 1: "))
 b = int(input("Enter number 2: "))
 c = int(input("Enter number 3: "))
